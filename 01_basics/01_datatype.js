22c1c601501a1d0788b3e00fc8ac1a948368443f
Rashmi Besra <rashmibesra111@gmail.com>
2024-10-28 11:48:53 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+let name="Rashmi"
+let age=18
+let isloogedIn=false
+let state=null
+//number=>2 to power53
+//bigint=
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>
+//symbol=>unique
+//object
+console.log(typeof "Rashmi");
+
+
+
