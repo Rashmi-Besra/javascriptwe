520421952c727d7648a8f11cf4ac0e3b4613eb97
Rashmi Besra <rashmibesra111@gmail.com>
2024-10-29 16:00:05 +0000

    conversionsofdatatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,33 @@
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueinnum=Number(score);
+// console.log(typeof valueinnum);
+// console.log(typeof(valueinnum));
+// console.log(valueinnum);
+
+let score=null;
+console.log(typeof score);
+console.log(typeof(score));
+let valueinnum=Number(score);
+console.log(typeof valueinnum);
+console.log(typeof(valueinnum));
+console.log(valueinnum);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+// let isloggedin=1;
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+//1=>true
+//0=>false
+//""=>false
+//"hitesh"=>true
+
+let somenum=33
+let stringnum=String(somenum);
+console.log(stringnum);
+console.log(typeof stringnum);
