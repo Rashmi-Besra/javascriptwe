ca0782b60805121e5ee3bbb4188fb539b3777690
Rashmi Besra <rashmibesra111@gmail.com>
2024-10-30 15:54:05 +0000

    Comparison

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -54,12 +54,15 @@ console.log(str3);
 // console.log("1"+2+2);
 // console.log(1+2+"2");
 
-console.log(+true);
-console.log(true+);
-console.log(+"");
-let num1,num2,num3
-num1=num2=num3=2+2
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+// let num1,num2,num3
+// num1=num2=num3=2+2
 
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
 
 
 
