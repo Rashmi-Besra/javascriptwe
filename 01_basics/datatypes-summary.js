948cf9a54d0678550851625c22b8fd9a349dfac6
Rashmi Besra <rashmibesra111@gmail.com>
2024-10-30 16:14:14 +0000

    datatypesummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+//primitive
+//7 types= string,Number,Boolean,Null,undefined,Symbol.BigInt
+
+const score=100
+const scorevalue=100.03
+const loggedin=falseconst
+const temp=null
+let useremail;
+const Id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(Id==anotherid);
+
+const bignum=4578123694785n
+
+
+//Reference Type(Non Primitive)
+//Array,objects,Functions
+
+const hero=["Shaktiman","naagraj"]
+let myobj={
+    name:"Rashmi",
+    age:20,
+}
+
+const myfun=function(){
+    console.log("Hello world");
+    
+}
+console.log(typeof temp)
