17aaacad4f526e182cde38ed400f814f7b008415
Rashmi Besra <rashmibesra111@gmail.com>
2024-11-05 10:45:46 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,49 @@
+//dates
+
+// let mydate=new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+// console.log(mydate.getTimezoneOffset());
+// console.log(mydate.getHours());
+// console.log(mydate.getMonth());
+
+// console.log(typeof mydate);
+// let mycreateddate=new Date(2023,0,23)
+// let mycreatreddate=new Date(2023,0,23,5,3)
+// let mycreateddate=new Date("2023-01-14");
+// let mycreateddate=new Date("01-15-2023")
+// console.log(mycreateddate.toDateString());
+
+//time
+let mytimestamp=Date.now()
+console.log(mytimestamp);
+console.log(Math.floor(Date.now()/1000));
+let newDate=new Date()
+console.log(newDate.getMonth()+1);
+ newDate.to localString('default',{
+    weekday: "long",
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
