10f65cace7af2fdbea0905e3afd81213f1911794
Rashmi Besra <rashmibesra111@gmail.com>
2024-11-14 16:35:16 +0000

    04-object

---
+++ 01_basics/04_object.js
@@ -0,0 +1,25 @@
+const tinderuser = new Object() //singleton
+const tinderuser ={} //non singleton oblect
+
+tinderuser.id="123abc"
+tinderuser.name="Sammy"
+tinderuser.isloggedin= false
+
+const regularuser ={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Rashmi",
+            lastname: "Besra"
+        }
+    }
+}
+
+//console.log(regularuser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4={5:"a",6:"b"}
+//const obj3={obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2,obj4)
+console.log(obj3);
