f4332fd50eb650ddcd9ff89eed4eb46904f94baf
Rashmi Besra <rashmibesra111@gmail.com>
2024-11-16 10:00:39 +0000

    object4

--- 01_basics/04_object.js
+++ 01_basics/04_object.js
@@ -1,5 +1,5 @@
 const tinderuser = new Object() //singleton
-const tinderuser ={} //non singleton oblect
+// const tinderuser ={} //non singleton oblect
 
 tinderuser.id="123abc"
 tinderuser.name="Sammy"
@@ -21,5 +21,24 @@ const obj1 = {1: "a",2:"b"}
 const obj2 = {3:"a",4:"b"}
 const obj4={5:"a",6:"b"}
 //const obj3={obj1,obj2}
-const obj3 = Object.assign({},obj1,obj2,obj4)
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3={...obj1,...obj2}
 console.log(obj3);
+
+const users=[
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email:"h@gmail.com"
+    },
+    {
+        id: 1,
+        email:"h@gmail.com"
+    },
+]
+
+users[1].email
+lo
